88380017119683dbcde864e7d2daebe2e58e39d7
ASMRSAEED <asmrsaeed@gmail.com>
2022-02-17 02:20:41 +0600

    Calculation Done

--- script/app.js
+++ script/app.js
@@ -1,1 +1,55 @@
-console.log('alart');
+
+// Income-Expenses Part
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const income = document.getElementById('income');
+    const foodExp = document.getElementById('food');
+    const rentExp = document.getElementById('rent');
+    const clotheExp = document.getElementById('clothes');
+
+    let expSum = parseInt(foodExp.value) + parseInt(rentExp.value) + parseInt(clotheExp.value);
+
+    // Total Expenses
+    let totalExp = document.getElementById('total-expenses');
+
+    totalExp.innerText = expSum;
+
+    // Balance
+    let balance = document.getElementById('balance');
+
+    let leftBalance = parseInt(income.value) - expSum;
+    console.log(leftBalance);
+
+    balance.innerText = leftBalance;
+
+})
+
+
+// Saving Part
+const savingAmount = document.getElementById('saving-amount');
+const remainingBalance = document.getElementById('remaining-balance');
+
+
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const saveInput = document.getElementById('save-input').value;
+
+
+    const extraBalanceRow = document.getElementById('balance');
+    const extraBalanceFreash = parseFloat(extraBalanceRow.innerText)
+
+    // Saving Percentage Calculation
+    const savingPercentage = (extraBalanceFreash * saveInput) / 100;
+    savingAmount.innerText = savingPercentage;
+
+
+    // Saving Amount and Remaining Balance
+    const finalremainingBalance = extraBalanceFreash - savingPercentage;
+    remainingBalance.innerText = finalremainingBalance;
+
+
+
+
+
+})
+
